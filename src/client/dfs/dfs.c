3c682c16f06c929cb3832b635a97df6d78c5ed3f
Mohamad Chaarawi <mohamad.chaarawi@intel.com>
2020-07-24 19:36:07 +0000

    DAOS-5349 DFS: add mode in DFS with DTX.
    For now this is enabled with an env variable (DFS_USE_DTX).
    In future patch we will add an explicit mount option and enable
    balanced mode by default.
    
    Signed-off-by: Mohamad Chaarawi <mohamad.chaarawi@intel.com>

--- src/client/dfs/dfs.c
+++ src/client/dfs/dfs.c
@@ -111,6 +111,8 @@ struct dfs_obj {
 struct dfs {
 	/** flag to indicate whether the dfs is mounted */
 	bool			mounted;
+	/** flag to indicate whether dfs is mounted with balanced mode (DTX) */
+	bool			use_dtx;
 	/** lock for threadsafety */
 	pthread_mutex_t		lock;
 	/** uid - inherited from container. */
@@ -152,6 +154,47 @@ struct dfs_entry {
 	/** Sym Link value */
 	char		*value;
 };
+
+#define TX_OPEN(d, t, f)						\
+do {									\
+	if (d->use_dtx) {						\
+		rc = daos_tx_open(d->coh, &t, f, NULL);			\
+		if (rc) {						\
+			D_ERROR("daos_tx_open() failed (%d)\n", rc);	\
+			D_GOTO(out, rc);				\
+		}							\
+	}								\
+} while (0)
+
+#define TX_COMMIT(d, t)							\
+do {									\
+	if (d->use_dtx) {						\
+		rc = daos_tx_commit(t, NULL);				\
+		if (rc == -DER_TX_RESTART) {				\
+			rc = daos_tx_restart(t, NULL);			\
+			if (rc) {					\
+				D_ERROR("daos_tx_restart() failed (%d)\n", \
+					rc);				\
+				D_GOTO(out, rc);			\
+			}						\
+			goto restart;					\
+		} else if (rc) {					\
+			D_ERROR("daos_tx_commit() failed (%d)\n", rc);	\
+			D_GOTO(out, rc);				\
+		}							\
+	}								\
+} while (0)
+
+#define TX_CLOSE(d, t)							\
+do {									\
+	if (daos_handle_is_valid(t)) {					\
+		rc = daos_tx_close(t,  NULL);				\
+		if (rc) {						\
+			D_ERROR("daos_tx_close() failed (%d)\n", rc);	\
+			D_GOTO(out, rc);				\
+		}							\
+	}								\
+} while (0)
 
 #if 0
 static void
@@ -1168,6 +1211,11 @@ dfs_mount(daos_handle_t poh, daos_handle_t coh, int flags, dfs_t **_dfs)
 	if (dfs == NULL)
 		D_GOTO(err_prop, rc = ENOMEM);
 
+	dfs->use_dtx = false;
+	d_getenv_bool("DFS_USE_DTX", &dfs->use_dtx);
+	if (dfs->use_dtx)
+		D_DEBUG(DB_ALL, "DFS mount with distributed transactions.\n");
+
 	dfs->poh = poh;
 	dfs->coh = coh;
 	dfs->amode = amode;
@@ -1281,6 +1329,7 @@ dfs_query(dfs_t *dfs, dfs_attr_t *attr)
 /* Structure of global buffer for dfs */
 struct dfs_glob {
 	uint32_t		magic;
+	bool			use_dtx;
 	int32_t			amode;
 	uid_t			uid;
 	gid_t			gid;
@@ -1297,6 +1346,7 @@ swap_dfs_glob(struct dfs_glob *dfs_params)
 	D_ASSERT(dfs_params != NULL);
 
 	D_SWAP32S(&dfs_params->magic);
+	D_SWAP32S(&dfs_params->use_dtx);
 	D_SWAP32S(&dfs_params->amode);
 	D_SWAP32S(&dfs_params->uid);
 	D_SWAP32S(&dfs_params->gid);
@@ -1357,6 +1407,7 @@ dfs_local2global(dfs_t *dfs, d_iov_t *glob)
 	/* init global handle */
 	dfs_params = (struct dfs_glob *)glob->iov_buf;
 	dfs_params->magic	= DFS_GLOB_MAGIC;
+	dfs_params->use_dtx	= dfs->use_dtx;
 	dfs_params->amode	= dfs->amode;
 	dfs_params->uid		= dfs->uid;
 	dfs_params->gid		= dfs->gid;
@@ -1422,6 +1473,7 @@ dfs_global2local(daos_handle_t poh, daos_handle_t coh, int flags, d_iov_t glob,
 
 	dfs->poh = poh;
 	dfs->coh = coh;
+	dfs->use_dtx = dfs_params->use_dtx;
 	dfs->amode = (flags == 0) ? dfs_params->amode : (flags & O_ACCMODE);
 	dfs->uid = dfs_params->uid;
 	dfs->gid = dfs_params->gid;
@@ -1562,6 +1614,9 @@ dfs_mkdir(dfs_t *dfs, dfs_obj_t *parent, const char *name, mode_t mode,
 
 	strncpy(new_dir.name, name, DFS_MAX_PATH);
 	new_dir.name[DFS_MAX_PATH] = '\0';
+
+	TX_OPEN(dfs, th, 0);
+restart:
 	rc = create_dir(dfs, th, parent->oh, cid, &new_dir);
 	if (rc)
 		D_GOTO(out, rc);
@@ -1576,8 +1631,9 @@ dfs_mkdir(dfs_t *dfs, dfs_obj_t *parent, const char *name, mode_t mode,
 		D_GOTO(out, rc);
 
 	daos_obj_close(new_dir.oh, NULL);
-
+	TX_COMMIT(dfs, th);
 out:
+	TX_CLOSE(dfs, th);
 	return rc;
 }
 
@@ -1677,6 +1733,8 @@ dfs_remove(dfs_t *dfs, dfs_obj_t *parent, const char *name, bool force,
 	if (rc)
 		return rc;
 
+	TX_OPEN(dfs, th, 0);
+restart:
 	/** Even with cond punch, need to fetch the entry to check the type */
 	rc = fetch_entry(parent->oh, th, name, false, &exists, &entry);
 	if (rc)
@@ -1720,9 +1778,11 @@ dfs_remove(dfs_t *dfs, dfs_obj_t *parent, const char *name, bool force,
 	if (rc)
 		D_GOTO(out, rc);
 
+	TX_COMMIT(dfs, th);
 	if (oid)
 		oid_cp(oid, entry.oid);
 out:
+	TX_CLOSE(dfs, th);
 	return rc;
 }
 
@@ -2254,6 +2314,8 @@ dfs_open(dfs_t *dfs, dfs_obj_t *parent, const char *name, mode_t mode,
 	obj->flags = flags;
 	oid_cp(&obj->parent_oid, parent->oid);
 
+	TX_OPEN(dfs, th, 0);
+restart:
 	switch (mode & S_IFMT) {
 	case S_IFREG:
 		rc = open_file(dfs, th, parent, flags, cid, chunk_size, obj);
@@ -2280,11 +2342,12 @@ dfs_open(dfs_t *dfs, dfs_obj_t *parent, const char *name, mode_t mode,
 		D_ERROR("Invalid entry type (not a dir, file, symlink).\n");
 		D_GOTO(out, rc = EINVAL);
 	}
-
+	TX_COMMIT(dfs, th);
 	*_obj = obj;
 
 	return rc;
 out:
+	TX_CLOSE(dfs, th);
 	D_FREE(obj);
 	return rc;
 }
@@ -3325,6 +3388,8 @@ dfs_move(dfs_t *dfs, dfs_obj_t *parent, char *name, dfs_obj_t *new_parent,
 	if (rc)
 		return rc;
 
+	TX_OPEN(dfs, th, 0);
+restart:
 	rc = fetch_entry(parent->oh, th, name, true, &exists, &entry);
 	if (rc) {
 		D_ERROR("Failed to fetch entry %s (%d)\n", name, rc);
@@ -3422,8 +3487,10 @@ dfs_move(dfs_t *dfs, dfs_obj_t *parent, char *name, dfs_obj_t *new_parent,
 		D_ERROR("Punch entry %s failed (%d)\n", name, rc);
 		D_GOTO(out, rc = daos_der2errno(rc));
 	}
+	TX_COMMIT(dfs, th);
 
 out:
+	TX_CLOSE(dfs, th);
 	if (entry.value) {
 		D_ASSERT(S_ISLNK(entry.mode));
 		D_FREE(entry.value);
@@ -3473,6 +3540,8 @@ dfs_exchange(dfs_t *dfs, dfs_obj_t *parent1, char *name1, dfs_obj_t *parent2,
 	if (rc)
 		return rc;
 
+	TX_OPEN(dfs, th, 0);
+restart:
 	rc = fetch_entry(parent1->oh, th, name1, true, &exists, &entry1);
 	if (rc) {
 		D_ERROR("Failed to fetch entry %s (%d)\n", name1, rc);
@@ -3521,8 +3590,10 @@ dfs_exchange(dfs_t *dfs, dfs_obj_t *parent1, char *name1, dfs_obj_t *parent2,
 		D_ERROR("Inserting entry %s failed (%d)\n", name2, rc);
 		D_GOTO(out, rc);
 	}
+	TX_COMMIT(dfs, th);
 
 out:
+	TX_CLOSE(dfs, th);
 	if (entry1.value) {
 		D_ASSERT(S_ISLNK(entry1.mode));
 		D_FREE(entry1.value);
